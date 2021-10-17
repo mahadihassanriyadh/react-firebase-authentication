3ab1dea2c121cd03974f2e90a7d42026945c5906
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-10-17 23:50:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@restart/ui/esm/Button';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { Col, Form, Row } from 'react-bootstrap';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
+
 
 function App() {
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  const handleSignUp = e => {
+    console.log('Sign Up Bhai');
+    e.preventDefault();
+  }
+  const handleEmailChange = e => {
+    console.log(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onSubmit={handleSignUp} className="mt-5 me-5 ms-5">
+        <h3 className="my-4 text-primary">Please Sign Up</h3>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control onChange={handleEmailChange} type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Form.Check label="Remember me" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit" className="btn btn-primary">Sign in</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+      <div>
+        <br /><br /><br /> <br /><br /><br />
+      </div>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhCN0QosNGQj5AL6zPIWiNcGWinuv41mU",
+    authDomain: "react-firebase-authentic-51257.firebaseapp.com",
+    projectId: "react-firebase-authentic-51257",
+    storageBucket: "react-firebase-authentic-51257.appspot.com",
+    messagingSenderId: "389946701828",
+    appId: "1:389946701828:web:53f5ad2e7cbc8899009d51",
+    measurementId: "G-G2PQRELCVD"
+  };
+
+export default firebaseConfig;
